cb433e67cd0fbeb283ca9beb535a8d40cb247126
sean-code <john.nganga@student.moringaschool.com>
2023-03-14 21:51:09 +0300

    ➕Add: Implement the use of puts at the beginning and ending of the process of seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Seeding has begun...🌱 "
+
+10.times
+    
+
+
+
+end
+
+
+puts "Seeding was succesful🌱 "
