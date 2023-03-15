595b516727156fcf3aa05f73b5d6ea6189e568bd
sean-code <john.nganga@student.moringaschool.com>
2023-03-15 16:27:32 +0300

    🚨Bug: Restore order by dropping the db, rerunning migrations and replanting

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,2 @@
 class Todo < ApplicationRecord
-    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_113226) do
   create_table "todos", force: :cascade do |t|
     t.text "title"
     t.text "description"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,5 +28,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_113226) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "todos", "users"
 end
